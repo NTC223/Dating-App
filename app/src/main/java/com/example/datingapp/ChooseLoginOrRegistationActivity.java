d14e8222c55988c488638b8381f6e6bacdf7503f
NTC224 <thanhcong19092004@gmail.com>
2024-10-25 23:33:16 +0700

    add: neu dang login vao thang mainAcivity

--- app/src/main/java/com/example/datingapp/ChooseLoginOrRegistationActivity.java
+++ app/src/main/java/com/example/datingapp/ChooseLoginOrRegistationActivity.java
@@ -11,12 +11,23 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class ChooseLoginOrRegistationActivity extends AppCompatActivity {
 
     private Button mLogin, mRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(ChooseLoginOrRegistationActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         setContentView(R.layout.activity_choose_login_or_registation);
 
         mLogin = (Button) findViewById(R.id.login);
