18fdb51d922c6077cac1aa62e73b3cce5dd2d703
milometqua <quynhhahahihibeep@gmail.com>
2024-10-26 23:33:17 +0700

    test

--- app/src/main/java/com/example/datingapp/MainActivity.java
+++ app/src/main/java/com/example/datingapp/MainActivity.java
@@ -241,12 +241,4 @@ public class MainActivity extends AppCompatActivity {
         finish();
         return;
     }
-
-    public void goToSettings(View view){
-        Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-        intent.putExtra("userSex", userSex);
-        startActivity(intent);
-        finish();
-        return;
-    }
 }
