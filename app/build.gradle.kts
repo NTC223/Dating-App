df018113a8910dfbeed228b5772e2a69602a4037
MinhAnh <minhanhjp38@gmail.com>
2024-11-14 17:21:00 +0900

    fix:xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,8 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    implementation ("de.hdodenhof:circleimageview:3.1.0")
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
