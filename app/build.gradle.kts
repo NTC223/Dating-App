6610a62c4b27fdf2fa098eeeffc4df2e64cacafe
Nguyễn Thành Công <thanhcong19092004@gmail.com>
2024-11-14 16:26:22 +0700

    fix: all

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,6 +50,7 @@ dependencies {
     implementation("com.google.android.material:material:1.8.0")
     implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 
+    implementation (libs.roundedimageview)
     implementation ("de.hdodenhof:circleimageview:3.1.0")
 
     implementation(libs.androidx.core.ktx)
